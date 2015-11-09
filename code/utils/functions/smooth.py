0f284b6b24b0aeb20b2546e66dbb7c4d88acb523
Rachel Lee <rachel@airbears2-10-142-60-186.airbears2.1918.berkeley.edu>
2015-11-08 16:06:44 -0800

    smoothing function fix

--- code/utils/functions/smooth.py
+++ code/utils/functions/smooth.py
@@ -1,13 +1,12 @@
 import numpy as np
 import itertools
-<<<<<<< HEAD
+
 from scipy.ndimage.filters import gaussian_filter
 
-def smooth(data_4d, sigma, time):
-=======
 
-def smooth_easy(data_4d):
->>>>>>> 3a31cef884a8fa8e517d99d9878dae0c0c9fccb2
+
+def smooth(data_4d, sigma, time):
+
 	"""
 	Return a 'smoothed' version of data_4d.
 
@@ -16,7 +15,7 @@ def smooth_easy(data_4d):
 	data_4d : numpy array of 4 dimensions 
         The image data of one subject
 
-<<<<<<< HEAD
+
     sigma : width of normal gaussian curve
 
     time : time slice (4th dimension)
@@ -30,86 +29,3 @@ def smooth_easy(data_4d):
 	smooth_results = scipy.ndimage.filters.gaussian_filter(time_slice, sigma)
 	return smooth_results
 
-=======
-	Returns
-    -------
-    smooth_results : numpy array of the smoothed data from data_4d
-	"""
-	return None
-	# for i in range 1 to (total number of voxels - 1)
-	# for i in range(1, ):
-		# take the average with two neighbors from each side, replace value at those indices with new avg.
->>>>>>> 3a31cef884a8fa8e517d99d9878dae0c0c9fccb2
-def smooth_advanced(data_4d, factor=4):
-	"""
-	Return a 'smoothed' version of data_4d.
-
-	Parameters
-	----------
-	data_4d : numpy array of 4 dimensions 
-        The image data of one subject
-    factor : the factor by which to reduce data_4d into factor-cubed chunks
-    	Smoothing factor
-
-    Note that the number of voxels may not be evenly divisble into factor-cubed chunks.
-
-<<<<<<< HEAD
-    Returns 
-    -------
-    smooth_results : array of the smoothed data from data_4d
-=======
-    Returns
-    -------
-    smooth_results : numpy array of the smoothed data from data_4d
->>>>>>> 3a31cef884a8fa8e517d99d9878dae0c0c9fccb2
-	"""
-	# Cut down data to be evenly split into 4 x 4 x 4 chunks (use mod %)
-	x_size = data_4d.shape[0]//factor
-	y_size = data_4d.shape[1]//factor
-	z_size = data_4d.shape[2]//factor
-
-	# Initialize a return list of zeros, same size as data_4d
-	smooth_results = np.zeros(data_4d.shape)
-
-	# Iterate data_4d by 4 x 4 x 4 (defaut factor size) chunks
-	for x, y, z in itertools.product(*map(range, (x_size, y_size, z_size))):
-		# Extract the chunk of data to average over
-		data_chunk = data_4d[x*factor : x*factor + factor, y*factor : y*factor + factor, z*factor : z*factor + 1]
-		# Flatten data_chunk, then find the mean of this array
-		data_mean = np.mean(data_chunk)
-		# Insert the value data_mean for the indices of the chunk given by data_chunk into smooth_results
-		smooth_results[x*factor : x*factor + factor, y*factor : y*factor + factor, z*factor : z*factor + 1] = data_mean
-
-	# Need a way to average the values that don't get touched upon??? Maybe??
-
-<<<<<<< HEAD
-	return smooth_results	
-=======
-	return smooth_results
-
-def smooth_super_advanced(data_4d, cube_dim=4):
-	"""
-	Return a 'smoothed' version of data_4d by looking at a cube-chunk around each voxel.
-
-	Parameters
-	----------
-	data_4d : numpy array of 4 dimensions 
-        The image data of one subject
-    cube_dim : the length of one side of the cube that surrounds the voxel at hand
-    	Smoothing factor, will average the cube for new smoothed value
-
-    Idea: Wrap data_4d within a larger cube of zeros to take care of 'edge' voxel averages
-
-    Returns
-    -------
-    smooth_results : numpy array of 4 dimensions of smoothed values of data_4d
-	"""
-	
-	# Initialize a return list of zeros, same size as data_4d
-	smooth_results = np.zeros(data_4d.shape)
-	# Average by the neighors of each voxel
-	# input must be 'odd' look at nodes
-	return None
->>>>>>> 3a31cef884a8fa8e517d99d9878dae0c0c9fccb2
-
-	
