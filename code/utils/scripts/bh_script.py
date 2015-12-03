f6f9d2f58b6b6f5edd6c1a3355e4c51c04cb6eb0
Rachel Lee <rachel6394@berkeley.edu>
2015-12-03 10:01:18 -0800

    add mini-masking to bh_script. Clean up this file later

--- code/utils/scripts/bh_script.py
+++ code/utils/scripts/bh_script.py
@@ -236,3 +236,102 @@ plt.title('Slice with Significant p-values (Q = .5)')
 plt.savefig(location_of_images+"significant_p_slice5.png")
 plt.close()
 print("# ==== END plot with Q = .5 done. === #")
+
+
+# =================================================================== #
+
+print("# ==== begin Ben's stuff ==== #")
+mask = nib.load(pathtodata + '/anatomy/inplane001_brain_mask.nii.gz')
+mask_data = mask.get_data()
+rachels_ones = np.ones((64, 64, 34))
+fitted_mask = make_mask(rachels_ones, mask_data, fit = True)
+fitted_mask[fitted_mask>0]=1 # corrected so there are only zeros and ones:
+# plt.hist(np.ravel(fitted_mask))
+# Another approach to masking
+mask_new=mask_data[::2,::2,:]
+
+assert(mask_new.shape==fitted_mask.shape)
+### still going to use fitted_mask
+def masking_reshape_start(data,mask):
+	"""
+	takes a 3 or 4d data and utilizes a mask to return a 1 or 2d reshaped output
+
+	Input:
+	------
+	data: 3d *or* 4d np.array  (x,y,z) or (x,y,z,t) shape
+	mask: a 3d np array  (x,y,z) shape, with values 0s and 1s (1 desired, 0 remove)
+
+	Returns:
+	--------
+	reshaped: a 1d *or* 2d np.array (connected to 3d or 4d "data" input)
+
+	"""
+	assert(len(data.shape) == 3 or len(data.shape) == 4)
+
+	if len(data.shape) == 3:
+		data_1d= np.ravel(data)
+		reshaped = data_1d[np.ravel(mask==1)]
+
+	if len(data.shape) == 4:
+		data_2d=data.reshape((-1,data.shape[-1]))
+		reshaped = data_2d[np.ravel(mask==1),:]
+	return reshaped
+
+
+
+def masking_reshape_end(data_small,mask,off_value=0):
+	"""
+	takes a 1d input, utilizes a mask to convert into 3d output ()
+
+	Notes:
+	------
+	mask must have same number of ones as the data_small.shape[0]
+
+	Input:
+	------
+	data_small: a 1d np.array
+	mask:       a 3d np array  (x,y,z) shape, with values 0s and 1s (1 desired, 0 remove), see notes
+	off_value:  the value to be replaced for the non-on values of the mask
+
+
+	Returns:
+	--------
+	data_big: 3d np.array  (x,y,z) shape
+
+	"""
+	assert(len(data_small.shape)==1)
+
+	data_big = off_value*np.ones((mask.shape))
+
+	data_big[mask==1]= data_small
+
+	return data_big
+p_vals_3d = p_vals.reshape((64,64,34))
+
+to_1d = masking_reshape_start(p_vals_3d,mask_new)
+
+for Q in [.05,.1,.25,.3,.4,.45,.49]:
+	bh_1d=bh_procedure(to_1d, Q)
+	to_3d = 2*masking_reshape_end(bh_1d,mask_new,.5)-1
+	plt.imshow(present_3d(to_3d),interpolation='nearest', cmap='seismic')
+	plt.title(Q)
+	plt.colorbar()
+	plt.figure()
+
+plt.figure()
+test= to_3d
+# maybe some clustering or smoothing?
+
+
+off=np.max(test)
+for i in 1+np.arange(62):
+	for j in 1+np.arange(62):
+		for k in 1+np.arange(32):
+			if np.sum(to_3d[(i-1):(i+2),(j-1):(j+2),(k-1):(k+2)] <0) <5 and to_3d[i,j,k]<0:
+				test[i,j,k]=off
+				
+
+plt.imshow(present_3d(test),interpolation='nearest', cmap='seismic')
+plt.title(str(Q)+ " mini-smoothing")
+plt.savefig(location_of_images+"ben_plot_bh.png")
+plt.colorbar()
